ffb78e49eb718f590fe13499b077e76fe5189f63
moonlight1000 <nawel.chekkal@gmail.com>
2023-05-16 12:24:27 +0200

    nouvele branche conditions + page conditions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { ResultatsComponent } from './resultats/resultats.component';
+import { NewConditionsComponent } from './new-conditions/new-conditions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ResultatsComponent
+    ResultatsComponent,
+    NewConditionsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/new-conditions/new-conditions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-conditions',
+  templateUrl: './new-conditions.component.html',
+  styleUrls: ['./new-conditions.component.css']
+})
+export class NewConditionsComponent {
+
+}
