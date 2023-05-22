b1cebdd312996616f2b4ada745b016b862d59bf3
Solange <solange@MBP-de-Solange.lan>
2023-05-22 15:20:05 +0200

    ajout des graphiques

--- src/app/resultats/resultats.component.ts
+++ src/app/resultats/resultats.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-resultats',
   templateUrl: './resultats.component.html',
   styleUrls: ['./resultats.component.css']
 })
-export class ResultatsComponent {
+export class ResultatsComponent{
+  public options: any = {
+    Chart: {
+      type: 'area',
+      height: 700
+    },
+    title: {
+      text: 'Graphique d évolution'
+    },
+    credits: {
+      enabled: false
+    },
+    xAxis: {
+      categories: ['janvier', 'février', 'mars', 'avril'],
+      tickmarkPlacement: 'on',
+      title: {
+          enabled: false
+      }
+  },
+    series: [{
+      name: 'Dooble',
+      data: [11, 8, 12, 15]
+  }]
+  }
+  public options2: any = {
+    Chart: {
+      type: 'area',
+      height: 700
+    },
+    title: {
+      text: 'Graphique d&#39évolution'
+    },
+    credits: {
+      enabled: false
+    },
+    xAxis: {
+      categories: ['janvier', 'février', 'mars', 'avril'],
+      tickmarkPlacement: 'on',
+      title: {
+          enabled: false
+      }
+  },
+    series: [{
+      name: 'Histoire d&#39orthographe',
+      data: [3, 6, 7, 10]
+  }]
+  }
+
+  constructor() { }
+  ngOnInit() {
+    Highcharts.chart('container2', this.options),
+    Highcharts.chart('container1', this.options2);
+  }
+
+
 
 }
