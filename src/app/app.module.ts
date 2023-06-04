1769c17db93b838d178286fedb8e25afae684a34
moonlight1000 <nawel.chekkal@gmail.com>
2023-06-04 14:07:57 +0200

    liaison avec la bdd part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
@@ -16,7 +18,9 @@ import { InscriptionComponent } from './inscription/inscription.component';
     InscriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-inscription',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent {
+  prenom: string = '';
+  nom: string = '';
+  email: string = '';
+  dateNaissance: string = '';
+  motDePasse: string = '';
+  confirmationMotDePasse: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  submitForm() {
+    console.log('Soumission du formulaire');
+    // Crée un objet contenant les données à envoyer
+    const userData = {
+      prenom: this.prenom,
+      nom: this.nom,
+      email: this.email,
+      dateNaissance: this.dateNaissance,
+      motDePasse: this.motDePasse
+    };
+
+    // Envoie les données à ton serveur en utilisant une requête HTTP
+    this.http.post('http://localhost:3000/user', userData)
+      .subscribe(
+        response => {
+          console.log('Inscription réussie !');
+          // Gère la réponse du serveur après l'inscription réussie, si nécessaire
+        },
+        error => {
+          console.error('Erreur lors de l\'inscription :', error);
+          // Gère les erreurs lors de l'inscription, si nécessaire
+        }
+      );
+  }
 }
